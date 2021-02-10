25d7ee3be7dfdd241fc30345813a0f90f4e08c6b
Matheus Afonso <matheusafonsouza@gmail.com>
2021-02-10 18:14:04 -0300

    fix: fixed response on product index

--- src/modules/products/infra/http/controllers/ProductsController.ts
+++ src/modules/products/infra/http/controllers/ProductsController.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express';
 
 export default class ProductController {
   async index(request: Request, response: Response): Promise<Response> {
-    return response.json(200).json({ ok: 'product ok!' });
+    return response.status(200).json({ ok: 'product ok!' });
   }
 }
