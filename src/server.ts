0223b856bbfd54958a2d36d52762c361f663410b
Matheus Afonso <matheusafonsouza@gmail.com>
2021-02-10 17:12:04 -0300

    inital commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (request: Request, response: Response) => {
+  return response.status(200).json({ ok: true });
+});
+
+app.listen(3333, () => {
+  console.log('Server running on port 3333!')
+});
