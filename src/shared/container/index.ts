33e5229d5b544e565cefe5b426aa9ef08f63a5ba
Matheus Afonso <matheusafonsouza@gmail.com>
2021-02-12 23:52:47 -0300

    feat: created container for skus repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,12 @@
 import { container } from 'tsyringe';
 import ProductsRepository from '../../modules/products/infra/typeorm/repositories/ProductsRepository';
 import IProductsRepository from '../../modules/products/repositories/IProductsRepository';
+import SkusRepository from '../../modules/skus/infra/typeorm/repositories/SkusRepository';
+import ISkuRepository from '../../modules/skus/repositories/ISkusRepository';
 
 container.registerSingleton<IProductsRepository>(
   'ProductsRepository',
   ProductsRepository,
 );
+
+container.registerSingleton<ISkuRepository>('SkusRepository', SkusRepository);
