03d486019fadc907cdb396dea6030566fc0712e2
Matheus Afonso <matheusafonsouza@gmail.com>
2021-02-12 23:50:42 -0300

    fix: create route for sku

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import productRouter from '../../../../modules/products/infra/http/routes/products.routes';
+import skuRouter from '../../../../modules/skus/infra/http/routes/skus.routes';
 
 const routes = Router();
 
 routes.use('/product', productRouter);
+routes.use('/sku', skuRouter);
 
 export default routes;
