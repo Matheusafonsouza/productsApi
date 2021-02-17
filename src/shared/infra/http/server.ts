a1dbdfb6cefdc8609f91b531cc616c2a635d4f85
matheusafonsouza <matheusafonsouza@gmail.com>
2021-02-17 20:48:46 -0300

    fix: realy fixed errors handling

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import cors from 'cors';
 import routes from './routes';
-
-import 'express-async-errors';
 
 import '../typeorm';
 import '../../container';
